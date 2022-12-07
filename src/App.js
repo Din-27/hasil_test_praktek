6853e4a6b2df70673ea007aed8f6008d25caebee
unknown <Herdiyana-gif>
2022-12-07 11:24:19 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import { useCallback } from 'react';
+import { memo } from 'react';
 
 function App() {
+  let alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  const patternRangoli = useCallback((x, y) => {
+    for (let index = 0; index < y; index++) {
+      if (index % parseFloat(y) === 0) {
+        console.log(`---------${alpha[y]}--------- \n`)
+      }
+      if (index % 3 === 0) {
+        console.log(`---------${alpha[y]}-${x}-${alpha[y]}--------- \n`)
+      }
+      if (index % 5 === 0) {
+        console.log(`---------${alpha[y]}-${x}-${x}-${x}-${alpha[y]}--------- \n`)
+      }
+      if (index % 7 === 0) {
+        console.log(`---------${alpha[y]}-${x}-${x}-${x}-${x}-${x}-${alpha[y]}--------- \n`)
+      }
+      if (index % 9 === 0) {
+        console.log(`---------${alpha[y]}-${x}-${x}-${x}-${x}-${x}-${x}-${x}-${alpha[y]}--------- \n`)
+      }
+      if (index % 11 === 0) {
+        console.log(`---------${alpha[y]}-${x}-${x}-${x}-${x}-${x}-${x}-${x}-${x}-${x}-${alpha[y]}--------- \n`)
+      }
+      if (index % 9 === 0) {
+        console.log(`---------${alpha[y]}-${x}-${x}-${x}-${x}-${x}-${x}-${x}-${alpha[y]}--------- \n`)
+      }
+      if (index % 7 === 0) {
+        console.log(`---------${alpha[y]}-${x}-${x}-${x}-${x}-${x}-${alpha[y]}--------- \n`)
+      }
+      if (index % 5 === 0) {
+        console.log(`---------${alpha[y]}-${x}-${x}-${x}-${alpha[y]}--------- \n`)
+      }
+      if (index % 5 === 0) {
+        console.log(`---------${alpha[y]}-${x}-${x}-${x}-${alpha[y]}--------- \n`)
+      }
+      if (index % 3 === 0) {
+        console.log(`---------${alpha[y]}-${x}-${alpha[y]}--------- \n`)
+      }
+      if (index % y === 0) {
+        console.log(`---------${alpha[y]}--------- \n`)
+      }
+      return index
+    }
+  }, [alpha])
+
+  useEffect(() => {
+    patternRangoli('j', 5)
+  })
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          { }<code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
